87aea511957d227afb55da938b24d5ddccadfd58
Struskawka <indi.15.15@gmail.com>
2022-12-06 08:42:42 +0000

    nic

--- foo.hpp
+++ foo.hpp
@@ -10,3 +10,14 @@ std::vector< char > foo(std::list< Human >& people)
     // Twoja implementacja tutaj
     return {};
 }
+/*Twoim zadaniem jest napisanie w pliku `foo.hpp` funkcji `foo`, która:
+
+- Przyjmuje przez referencję listę obiektów typu `Human`.
+- Zwraca wektor obiektów typu `char`.
+- Inkrementuje pole `age` wszystkich obiektów w podanej liście poprzez zawołanie metody `birthday`.
+- Do zwracanego wektora wpisuje znaki `y` lub `n` (yes/no) w zależności od tego, czy dana osoba lubi co najmniej jedno ze zbioru zwierząt {pies, kot} (`y` jeżeli tak, `n` jeżeli nie).
+
+Kolejność znaków powinna być odwrotna od kolejności osób.
+Do sprawdzenia warunku użyj metody `isMonster`.
+Innymi słowy, jeżeli metoda `isMonster` *i*-tej osoby na liście zwróci wartość `true`, to (*n* - *i*)-ty element zwracanego wektora powinien mieć wartość `n`
+*/
